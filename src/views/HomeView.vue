0ea5bdb1206bd8bcc140b89af0228700b43a2125
Miguel Hidalgo <hpmiguel@hotmail.com>
2024-06-30 13:29:10 +0200

    First app version

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import Table from '@/components/Table.vue'
+</script>
+
+<template>
+  <Navbar></Navbar>
+  <Table></Table>
+</template>
